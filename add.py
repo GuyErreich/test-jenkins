afc2e24d32a9ffb5913d097cab9373369be1a4a8
GuyErreich <guyerreich@gmail.com>
2021-02-16 09:12:35 +0000

    Added the func and the test

---
+++ add.py
@@ -0,0 +1,5 @@
+#!/user/bin/python
+
+def add(a, b):
+    sum = a + b
+    return sum
---
+++ test_add.py
@@ -0,0 +1,8 @@
+#!/user/bin/python
+
+from add import add
+
+def test_add():
+    sum = add(4,5)
+    assert (sum == 9)
+
